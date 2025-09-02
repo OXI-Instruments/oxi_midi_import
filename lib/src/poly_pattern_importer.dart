4601e42a3f69dba3aab6d9054f22acdd57f134ff
artotaku <artotaku@gmx.de>
2025-09-02 14:05:59 +0200

    feat(add-reason-test-cases): add a test case for poly pattern importer

--- lib/src/poly_pattern_importer.dart
+++ lib/src/poly_pattern_importer.dart
@@ -1,34 +1,38 @@
 import 'dart:io';
 import 'dart:math';
+
 import 'package:dart_midi_pro/dart_midi_pro.dart';
 import 'package:oxi_midi_import/oxi_midi_import.dart';
-import 'package:oxi_midi_import/src/models/note.dart' show Note;
 import 'package:oxi_midi_import/src/utilities/midi_event_helpers.dart';
 import 'package:oxi_midi_import/src/utilities/midi_to_pattern_converter.dart';
 
 final class PolyPatternImporter {
-  static const _maxPatternLength = 1000000;
 
   static Future<PolyPattern> importPattern(File file) async {
     final bytes = await file.readAsBytes();
+    //final name = FileHelpers.getFileNameWithoutExtension(result);
 
     final midi = MidiParser().parseMidiFromBuffer(bytes);
     final ticksPerStep = (midi.header.ticksPerBeat ?? 96) ~/ 4;
 
-    final List<Note> notes = [];
-    int length = _maxPatternLength; // NOTE: some big number
-    for (final track in midi.tracks) {
-      notes.addAll(MidiEventHelpers.notesFromMidiEvents(track));
-      final trackLength = max(
-        MidiEventHelpers.getTrackLength(track),
-        ticksPerStep * 8, // 8 steps minimum
-      );
-      length = min(length, trackLength);
+    int trackIndex = midi.tracks.indexWhere((track) => track.any((event) => event is NoteOnEvent));
+    if (trackIndex == -1) {
+      trackIndex = 0;
     }
-    if (length == _maxPatternLength) {
-      throw Exception('MIDI clip is too long or invalid.');
+    int length = max(
+      MidiEventHelpers.getTrackLength(midi.tracks[trackIndex]),
+      ticksPerStep * 8, // 8 steps minimum
+    );
+
+    List<MidiEvent> midiEvents = midi.tracks[trackIndex];
+
+    if (ticksPerStep != 24) {
+      midiEvents = MidiEventHelpers.retimeMidiEvents(midiEvents, 24, ticksPerStep);
+      length = (length * 24 / ticksPerStep).ceil();
     }
-    final pattern = await MidiToPatternConverter.createPolyPatternFrom(notes, length);
+
+    final notes = MidiEventHelpers.notesFromMidiEvents(midiEvents, ticksPerStep: ticksPerStep);
+    final pattern = MidiToPatternConverter.createPolyPatternFrom(notes, length);
 
     return pattern;
   }
