b53dc75ff9b0126ab79f1f04fe6dc340559fd1d9
rmri <intonarumori@gmail.com>
2025-09-02 15:17:04 +0200

    cleanup

--- lib/src/poly_pattern_importer.dart
+++ lib/src/poly_pattern_importer.dart
@@ -7,22 +7,21 @@ import 'package:oxi_midi_import/src/utilities/midi_event_helpers.dart';
 import 'package:oxi_midi_import/src/utilities/midi_to_pattern_converter.dart';
 
 final class PolyPatternImporter {
-  static const _maxPatternLength = 1000000;
+  static const _maxPatternLength = 1000000; // NOTE: some big number
+  static const _minimumStepCount = 8;
+  static const _defaultTicksPerStep = 24;
 
   static Future<PolyPattern> importPattern(File file) async {
     final bytes = await file.readAsBytes();
 
     final midi = MidiParser().parseMidiFromBuffer(bytes);
-    final ticksPerStep = (midi.header.ticksPerBeat ?? 96) ~/ 4;
+    final ticksPerStep = (midi.header.ticksPerBeat ?? _defaultTicksPerStep * 4) ~/ 4;
 
     final List<Note> notes = [];
-    int length = _maxPatternLength; // NOTE: some big number
+    int length = _maxPatternLength;
     for (final track in midi.tracks) {
       notes.addAll(MidiEventHelpers.notesFromMidiEvents(track));
-      final trackLength = max(
-        MidiEventHelpers.getTrackLength(track),
-        ticksPerStep * 8, // 8 steps minimum
-      );
+      final trackLength = max(MidiEventHelpers.getTrackLength(track), ticksPerStep * _minimumStepCount);
       length = min(length, trackLength);
     }
     if (length == _maxPatternLength) {
